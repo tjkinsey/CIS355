e8068227d945e89ffe5d76f1ba9801d0e98600d6
David Wells <dave1.t.wells@gmail.com>
2018-11-28 16:51:49 -0700

    Update teamScriptInsert.sql

--- teamScriptInsert.sql
+++ teamScriptInsert.sql
@@ -1,12 +1,14 @@
 USE ValleyVinyardsDaveIanTanner
 
+DELETE FROM PeopleAndCustomers;
 INSERT INTO PeopleAndCustomers
 	Values('Dave Wells', 6913143, '1236 Canvasback Ct.', 'dwells@rams.colostate.edu', 'V')
-	,('Bob Ross' 1234567, '1234 HeartsAndCandyLane.', 'BobThePainter@rams.colostate.edu' 'V')
-	,('Bob The Builder', '3456 IBuiltThisMfHouse!', 'BobTheBuilder@rams.colostate.edu', 'V')
+	,('Bob Ross', 1234567, '1234 HeartsAndCandyLane.', 'BobThePainter@rams.colostate.edu', 'V')
+	,('Bob The Builder', 9999999, '3456 IBuiltThisMfHouse!', 'BobTheBuilder@rams.colostate.edu', 'V')
 	,('Tanner Kinsey', 8889999, '111 Place Ave.', 'tkinsey@rams.colostate.edu', 'V');
 	
-INSERT INTO Vender
+DELETE FROM Vendor;
+INSERT INTO Vendor
 	Values(2,5551111, 'Super Awesome Bottles Co.','Best Green Bottles Around')
 	,(3,5552222, 'Big ol Bottles Inc.','Biggest Dang Bottles Money Can Buy')
 	,(4,5553333, 'Unbreakable Glass Co.', 'You seriously cannot break these bottles!')
